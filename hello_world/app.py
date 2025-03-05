f61c9820549cf996843eb4d9091ae226b72152ad
Zoran-Janjic <janjiczoran23@gmail.com>
2025-03-05 16:19:03 +0900

    Test AWS Code Pipeline 8

--- hello_world/app.py
+++ hello_world/app.py
@@ -35,7 +35,7 @@ def lambda_handler(event, context):
     mongodb_APIgateway_uri = os.getenv("documentDB_url")
 
     try:
-        print(f"This is the new try mnjau")
+        print(f"This is the new try mnjau 1")
 
         # Extract bucket name and key from the event
         bucket, key = extract_event_details(event)
